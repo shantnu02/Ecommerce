8cfc50de4dfdb8e48feee9c64ed534b6527646e4
SHANTANU SINGH <48873580+shantnu02@users.noreply.github.com>
2021-02-10 21:27:44 +0530

    Update Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -9,16 +9,18 @@ class Basket extends Component {
 
     return (
       <div className="alert alert-info">
-        {cartItems === 0 ? "Basket is empty" : 
+        {cartItems.length === 0 ? (
+          "Basket is empty"
+        ) : (
           <div>
-            You have {cartItems.map} items in the basket. <hr />
+            You have {cartItems.length} items in the basket. <hr />
           </div>
-        }
-        {cartItems  > 0 && cartItems &&
+        )}
+        {cartItems.length > 0 && (
           <div>
-            <ul  style={{ marginLeft: -25 }}>
-              {cartItems.map(item =>
-                <li>
+            <ul style={{ marginLeft: -25 }}>
+              {cartItems.map((item) => (
+                <li key={item.id}>
                   <b>{item.title}</b>
                   <button
                     style={{ float: "right" }}
@@ -29,25 +31,28 @@ class Basket extends Component {
                   >
                     X
                   </button>
-                  
+                  <br />
+                  {item.count} X {util.formatCurrency(item.price)}
                 </li>
-                )}
+              ))}
             </ul>
 
-          <b>
-            Sum:{" "}   {util.formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0)
+            <b>
+              Sum:{" "}
+              {util.formatCurrency(
+                cartItems.reduce((a, c) => a + c.price * c.count, 0)
               )}
             </b>
-            <button className="btn btn-primary" onClick={() => alert("Todo: Implement checkout page.")}
-             className="btn btn-primary"
+            <button
+              onClick={() => alert("Todo: Implement checkout page.")}
+              className="btn btn-primary"
             >
-              Checkout
+              checkout
             </button>
           </div>
-        }
+        )}
       </div>
     );
-    
   }
  
 }
